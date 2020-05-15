1680445a9d3a6c1c950217f66d9e7a657a808d9a
josue rodriguez <Josue2535>
2020-05-14 19:48:32 -0500

    teest

--- src/Test/ALGraphTest.java
+++ src/Test/ALGraphTest.java
@@ -2,9 +2,12 @@ package Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import model.ALGraph;
+import model.Edge;
 import model.Station;
 
 public class ALGraphTest {
@@ -36,6 +39,34 @@ public class ALGraphTest {
 		m.connect(s3, s4, 15);
 		m.connect(s5, s4, 20);
 	}
+	private void stage2() {
+		Station s1;
+		Station s2;
+		Station s3;
+		Station s4;
+		Station s5;
+		Station s6;
+		m = new ALGraph<Station>();
+		s1 = new Station("a");
+		s2 = new Station("b");
+		s3 = new Station("c");
+		s4 = new Station("d");
+		s5 = new Station("e");
+		s6 = new Station("z");
+		m.add(s1);
+		m.add(s2);
+		m.add(s3);
+		m.add(s4);
+		m.add(s5);
+		m.add(s6);
+		m.connect(s1, s2, 4);
+		m.connect(s1, s4, 2);
+		m.connect(s2, s3, 3);
+		m.connect(s2, s5, 3);
+		m.connect(s4, s5, 3);
+		m.connect(s3, s6, 2);
+		m.connect(s5, s6, 1);
+	}
 
 	@Test
 	void DFSTest() {
@@ -52,6 +83,25 @@ public class ALGraphTest {
 	@Test
 	void floydWarshallTest() {
 		stage1();
-		assertEquals(m.prim(m.getVertexs().get(1).getObject()),0);
+		assertEquals(m.prim(m.getVertexs().get(0).getObject()),0);
+	}
+	@Test 
+	void kruskalTest() {
+		int camino =0;
+		stage1();
+		List<Edge<Station>> m1 =  m.Kruskal();
+		for (int i = 0; i < m1.size(); i++) {
+			camino += m1.get(i).getWeight();
+		}
+		assertTrue(camino==43);
+		
+		
+	}
+	@Test
+	void dijkstraTest() {
+		stage2();
+		m.Dijkstra(m.getVertexs().get(0).getObject());
+		int t = m.getVertexs().get(0).getDistance();
+		assertTrue(t==0);
 	}
 }
